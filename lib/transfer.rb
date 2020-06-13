5cf61374f4cb3835bf9e5806fb868c362087d08b
Michael Harris <mwharris7@gmail.com>
2020-06-13 12:09:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,13 +21,15 @@ class Transfer
 
   def execute_transaction
     if valid?
+      @transfer_counter += 1
       @sender.deposit(-@amount)
       @receiver.deposit(@amount)
       @status = "complete"
-      @transfer_counter += 1
+      
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
+      
     end
   end
 
