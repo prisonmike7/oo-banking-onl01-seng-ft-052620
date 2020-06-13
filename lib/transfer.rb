8a6b3448d27904cefdc0db3528bf3b15247a934e
Michael Harris <mwharris7@gmail.com>
2020-06-13 11:33:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -7,9 +7,12 @@ class Transfer
     @receiver = receiver
     @amount = amount
     @status = "pending"
+    sender.amount -= amount
+    receiver.amount += amount
   end
 
   def valid?(amount)
+
     if @sender.valid?(amount) || @receiver.valid?(amount)
       return true
     end
