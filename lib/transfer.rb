41ad608fb83c3b5f626e64f8a88539c611db20d6
Michael Harris <mwharris7@gmail.com>
2020-06-13 11:49:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
 
   def execute_transaction
     if valid?
-      @sender.balance -= @amount
+      @sender.balance += @amount
       @receiver.balance += @amount
       @status = "complete"
     end
